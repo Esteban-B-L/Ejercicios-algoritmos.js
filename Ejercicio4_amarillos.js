164b1edd8459f80821c509597fb9608b14d33355
Esteban-B-L <ebetancurt_739@unihumboldt.edu.co>
2025-02-23 07:31:11 -0500

    Cuarto ejercicio amarillo

---
+++ Ejercicio4_amarillos.js
@@ -0,0 +1,26 @@
+let Norte = false;
+let Sur = false;
+let Este = false;
+let Oeste = false;
+
+if(Norte)
+{
+console.log("Se está dirigiendo hacia el norte");
+}
+
+else if(Sur)
+{
+console.log("Se está dirigiendo hacia el sur");
+}
+else if(Este)
+{
+console.log("Se está dirigiendo hacia el este");
+}
+else if(Oeste)
+{
+console.log("Se está dirigiendo hacia el oeste");
+}
+else
+{
+console.log("No se está dirigiendo hacia ninguno de los 4 puntos cardinales básicos");
+}
