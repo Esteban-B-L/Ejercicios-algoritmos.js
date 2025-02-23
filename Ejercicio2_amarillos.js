3dc38311766f857e5618672821d7cf9aa12d9e15
Esteban-B-L <ebetancurt_739@unihumboldt.edu.co>
2025-02-23 06:47:22 -0500

    Segundo ejercicio amarillo

---
+++ Ejercicio2_amarillos.js
@@ -0,0 +1,15 @@
+let Pasajero_Avion = "Primera clase";
+
+if (Pasajero_Avion == "Primera clase")
+{
+console.log(`Los asientos y servicios de ${Pasajero_Avion} estan disponibles para usted`);
+}
+
+else if (Pasajero_Avion == "Normal clase")
+{
+console.log(`Los asientos y servicios de ${Pasajero_Avion} estan disponibles para usted`);
+}
+else 
+{
+console.log ("Usted no es pasajero de ningun vuelo, por favor realice una compra de boleto de avion y dirigase nuevamente al avion para ser uno");
+}
