b36aef215eca572050ebf5b44177bdc447232e4d
Esteban-B-L <ebetancurt_739@unihumboldt.edu.co>
2025-02-23 06:25:14 -0500

    Primer ejercicio amarillo y modificaciones al quinto ejercicio rojo

---
+++ Ejercicio1_amarillos.js
@@ -0,0 +1,6 @@
+let Agua_Temperatura = 110;
+
+if (Agua_Temperatura >= 100)
+{
+console.log(`El agua está hirviendo, su temperatura es de ${Agua_Temperatura} C°`);
+}
--- Ejercicio5_rojos.js
+++ Ejercicio5_rojos.js
@@ -1,6 +1,6 @@
-let LuzrojaLuzverde = "Luz verde";
+let Luzroja_Luzverde = "Luz verde";
 
-LuzrojaLuzverde == "Luz verde" ? console.log(`La ${LuzrojaLuzverde} está encendida, puede avanzar sin peligro`):
+Luzroja_Luzverde == "Luz verde" ? console.log(`La ${Luzroja_Luzverde} está encendida, puede avanzar sin peligro`):
 
-LuzrojaLuzverde == "Luz roja" ? console.log(`La ${LuzrojaLuzverde} está encendia, si avanza morirá`) :
-console.log(`mostrar ${LuzrojaLuzverde},la luz está dañada, espere instrucciones de los guardias`);
+    Luzroja_Luzverde == "Luz roja" ? console.log(`La ${Luzroja_Luzverde} está encendia, si avanza morirá`) :
+console.log(`mostrar ${Luzroja_Luzverde},la luz está dañada, espere instrucciones de los guardias`);
