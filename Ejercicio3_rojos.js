e84c0efb03a669f7d650aed46900024548abb947
Esteban-B-L <ebetancurt_739@unihumboldt.edu.co>
2025-02-23 01:40:55 -0500

    Tercer ejercicio

---
+++ Ejercicio3_rojos.js
@@ -0,0 +1,6 @@
+let prisionero = false;
+let NamePrisionero = "Charlie";
+
+let mensaje = prisionero ? `El prisionero llamado ${NamePrisionero} está preso"` : `El prisionero llamado ${NamePrisionero} está libre`
+
+console.log (mensaje);
