3f0d924af8354e16aad8a2b0a7138e5fa22bd9ce
Esteban-B-L <ebetancurt_739@unihumboldt.edu.co>
2025-02-23 12:15:50 -0500

    Primer ejercicio propuesto

---
+++ EjerciciosPropuestos_1.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Por favor ingrese su nombre: ", (Name) => {
+    rl.question("Por favor, ingrese el número de horas trabajadas: ", (Horas) => {
+        Horas = Number(Horas);
+        let Salario;
+
+        if (Horas <= 10 && Horas > 0) {
+            Salario = Horas * 30000;
+        } else if (Horas > 10) {
+            Salario = Horas * 33000;
+        } else {
+            Salario = 0; // Si Horas es 0 o menos, el salario es 0
+        }
+
+        console.log(`Señor/a ${Name}, el número de horas es ${Horas} y su salario equivale a $${Salario}.`);
+        rl.close();
+    });
+});
