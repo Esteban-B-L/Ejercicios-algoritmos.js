fc51c1c21fb97fcbcf053291de34db26fb141805
Esteban-B-L <ebetancurt_739@unihumboldt.edu.co>
2025-02-23 02:29:18 -0500

    Quinto ejercicio

---
+++ Ejercicio5_rojos.js
@@ -0,0 +1,6 @@
+let LuzrojaLuzverde = "Luz verde";
+
+LuzrojaLuzverde == "Luz verde" ? console.log(`La ${LuzrojaLuzverde} está encendida, puede avanzar sin peligro`):
+
+LuzrojaLuzverde == "Luz roja" ? console.log(`La ${LuzrojaLuzverde} está encendia, si avanza morirá`) :
+console.log(`mostrar ${LuzrojaLuzverde},la luz está dañada, espere instrucciones de los guardias`);
