08a18751ad603a4f32be5b2908c4df2cfbb2dd7a
Esteban-B-L <ebetancurt_739@unihumboldt.edu.co>
2025-02-23 02:02:25 -0500

    Cuarto ejercicio

---
+++ Ejercicio4_rojos.js
@@ -0,0 +1,6 @@
+const PlayerName = "Esteban B.L"
+const Password = "54321";
+
+PlayerName == "Esteban B.L" && Password == "54321" ? 
+console.log(`${PlayerName} bienvenido de vuelta al juego`) : 
+console.log(`nombre de usuario:${PlayerName} o contraseña: ${Password} incorrectos`);
